568b05709dabdb9e103e3542e5e3314a3e36b231
kashifsami31 <63497687+kashifsami31@users.noreply.github.com>
2020-04-11 13:45:43 +0200

    Add files via upload

---
+++ Capstone notebook 1.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#import pandas and numpy library
+import pandas as pd
+import numpy  as np
+
+
+# In[2]:
+
+
+print ("Hello Capstone Project Course!")
+
